894d1e021806d6e4267124ea75bf244823f7d1ce
J_M_Dominguez <jmdominguezhernandez@gmail.com>
2022-01-05 00:35:16 +0100

    AP01_Version_Entrega

--- App.js
+++ App.js
@@ -1,30 +1,5 @@
 import React, {Component} from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  Image,
-  TouchableOpacity,
-  ScrollView,
-  Dimensions,
-} from 'react-native';
-const screenWidth = Dimensions.get('window').width;
-const images = [
-  'https://images.unsplash.com/photo-1513721032312-6a18a42c8763?w=152&h=152&fit=crop&crop=faces',
-  'https://images.unsplash.com/photo-1511765224389-37f0e77cf0eb?w=125&h=125&fit=crop',
-  'https://images.unsplash.com/photo-1497445462247-4330a224fdb1?w=125&h=125&fit=crop',
-  'https://images.unsplash.com/photo-1426604966848-d7adac402bff?w=125&h=125&fit=crop',
-  'https://images.unsplash.com/photo-1502630859934-b3b41d18206c?w=125&h=125&fit=crop',
-  'https://images.unsplash.com/photo-1515023115689-589c33041d3c?w=125&h=125&fit=crop',
-  'https://images.unsplash.com/photo-1504214208698-ea1916a2195a?w=125&h=125&fit=crop',
-  'https://images.unsplash.com/photo-1515814472071-4d632dbc5d4a?w=125&h=125&fit=crop',
-  'https://images.unsplash.com/photo-1511407397940-d57f68e81203?w=125&h=125&fit=crop',
-  'https://images.unsplash.com/photo-1518481612222-68bbe828ecd1?w=125&h=125&fit=crop',
-  'https://images.unsplash.com/photo-1505058707965-09a4469a87e4?w=125&h=125&fit=crop',
-  'https://images.unsplash.com/photo-1423012373122-fff0a5d28cc9?w=125&h=125&fit=crop',
-];
-
-const localImg = require('./images/1.1.jpg');
+import {StyleSheet, View, Text, Image} from 'react-native';
 
 export default class App extends Component {
   render() {
@@ -38,11 +13,19 @@ export default class App extends Component {
             <View style={styles.container8}>
               <View style={styles.datos}>
                 <Text
-                  style={{textAlign: 'left', fontWeight: 'bold', fontSize: 12}}>
+                  style={{
+                    textAlign: 'left',
+                    fontWeight: 'bold',
+                    fontSize: 12,
+                  }}>
                   Posts
                 </Text>
                 <Text
-                  style={{textAlign: 'left', fontWeight: 'bold', fontSize: 14}}>
+                  style={{
+                    textAlign: 'left',
+                    fontWeight: 'bold',
+                    fontSize: 14,
+                  }}>
                   20
                 </Text>
               </View>
@@ -58,16 +41,23 @@ export default class App extends Component {
               </View>
               <View style={styles.datos2}>
                 <Text
-                  style={{textAlign: 'left', fontWeight: 'bold', fontSize: 12}}>
+                  style={{
+                    textAlign: 'left',
+                    fontWeight: 'bold',
+                    fontSize: 12,
+                  }}>
                   following 1103
                 </Text>
               </View>
             </View>
 
-                    <View style={styles.boton}>
-                        
-                        
-              <Text style={{fontWeight: 'bold', color: 'black', fontSize: 14}}>
+            <View style={styles.boton}>
+              <Text
+                style={{
+                  fontWeight: 'bold',
+                  color: 'black',
+                  fontSize: 14,
+                }}>
                 Edit Profile
               </Text>
             </View>
